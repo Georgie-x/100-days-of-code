b22a0af974164450c05006ee3f1e5c1736be8a42
Georgie <georgiex@gmail.com>
2025-09-23 00:00:32 +0100

    day 0 webpage created

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+function App() {
+	return (
+		<div>
+			<h1>Georgina Rimmer - 100 days of code</h1>
+			<h2>Day 0</h2>
+			<p>
+				Created readme for Flagit and hosted on portfolio
+				<a href='www.flagit.georgiex.co.uk'> Go to FlagIt</a>. Created this webpage and will host tomorrow.
+			</p>
+		</div>
+	)
+}
+
+export default App
