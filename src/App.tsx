e97c539879d2edd85974d224f8de5fd26118f30c
Georgie <georgiex@gmail.com>
2025-09-23 23:41:58 +0100

    added some CSS and day 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 function App() {
 	return (
 		<div>
@@ -6,7 +5,13 @@ function App() {
 			<h2>Day 0</h2>
 			<p>
 				Created readme for Flagit and hosted on portfolio
-				<a href='www.flagit.georgiex.co.uk'> Go to FlagIt</a>. Created this webpage and will host tomorrow.
+				<a href='www.flagit.georgiex.co.uk'> Go to FlagIt</a>. Created this webpage and will host
+				tomorrow.
+			</p>
+			<h2>Day 1</h2>
+			<p>
+				I want to be able to deploy this page automatically so I've set up GitHub Actions using this
+				wizard <a href='https://github.com/marketplace/actions/deploy-to-hostinger'>GitHub Actions instructions</a>. It was a good wizard and everything easier than I imagined, so I hope it works &#128522; Also adding a bit of CSS for a black and white theme.
 			</p>
 		</div>
 	)
