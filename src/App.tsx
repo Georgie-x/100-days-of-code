ceab4c35379cdc56c9feb200191dc99a8f67149e
Georgie <georgiex@gmail.com>
2025-09-25 00:14:53 +0100

    doing day 2

--- src/App.tsx
+++ src/App.tsx
@@ -5,22 +5,26 @@ function App() {
 			<h2>Day 0</h2>
 			<p>
 				Created readme for previous project Flagit and hosted it on portfolio website{" "}
-				<a href='https://flagit.georgiex.co.uk' target="blank">Go to FlagIt Quiz</a>. Created this webpage and will
-				host tomorrow.
+				<a href='https://flagit.georgiex.co.uk' target='blank'>
+					Go to FlagIt Quiz
+				</a>
+				. Created this webpage and will host tomorrow.
 			</p>
 			<h2>Day 1</h2>
 			<p>
 				I want to be able to deploy this page automatically so I've set up GitHub Actions using this
 				wizard{" "}
-				<a href='https://github.com/marketplace/actions/deploy-to-hostinger' target="blank">
+				<a href='https://github.com/marketplace/actions/deploy-to-hostinger' target='blank'>
 					GitHub Actions instructions
 				</a>
 				. It was a good wizard and everything easier than I imagined, so I hope it works &#128522;
 				Also adding a bit of CSS for a black and white theme.
 			</p>
-					<h2>Day 3</h2>
+			<h2>Day 3</h2>
 			<p>
-				I had a few troubles getting the GitHub Actions to work (wrong paths and I presumed it would sort out branches automatically) but it is now working so I plan to update some of my other websites with it.
+				I had a few troubles getting the GitHub Actions to work (wrong paths and I presumed it would
+				sort out branches automatically) but it is now working so I plan to update some of my other
+				websites with it.
 			</p>
 		</div>
 	)
