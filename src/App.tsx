20186c391c358467c9d4e36d432ce0e17ddcb9a5
Georgie <georgiex@gmail.com>
2025-10-25 23:24:05 +0100

    day 33

--- src/App.tsx
+++ src/App.tsx
@@ -181,7 +181,8 @@ function App() {
 			<p>Read about Redux and Next.js, not sure which to try also there's Vue even. I'm not sure I want to get into anything confusing for this I just want to pick the most helpful.</p>
 			<h2>Day 32</h2>
 			<p>Reading React 19.2 docs to update myself as to what's changed. Doing a tutorial through their own IDE, but it's buggy making it doubly hard.</p>
-			
+			<h2>Day 33</h2>
+			<p>Refreshing React and doing little exercises off the main <a href="https://react.dev/learn/">React docs</a>
 		</div>
 	)
 }
